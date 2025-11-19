ff416d87133cd2dc8eb7980bdcf5add5a2a86200
Abdallah <77486784+Abdallah0799@users.noreply.github.com>
2025-11-19 17:43:37 +0100

    clean repo

--- dags/SNCF/core.py
+++
@@ -1,5 +0,0 @@
-# 1. Fetch file from url and load it to GCS bucket
-# => custom code
-# 2. Extract file from GCS, rename columns
-# 3. Create BigQuery dataset if it doesn't exist
-# 4. Load transformed data to BigQuery with airflow operator
